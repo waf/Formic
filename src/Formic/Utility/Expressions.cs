ee95a9447addd4d5eb67306e022540d6cf5e3f7a
Will Fuqua <wafuqua@gmail.com>
2016-03-21 09:40:14 +0700

    comments and code cleanup

--- src/Formic/Utility/Expressions.cs
+++ src/Formic/Utility/Expressions.cs
@@ -10,25 +10,41 @@ namespace Formic.Utility
 {
     public class Expressions
     {
-        public static IQueryable<object> FilterByEntityParameters(IQueryable<object> allData, IEntityType entity, IDictionary<string, string[]> parameters)
+        /// <summary>
+        /// filter the provided queryable with the provided parameters.
+        /// e.g. given parameters {"property1": ["val1", "val2"], "property2": ["val3"]}
+        ///      return a filtered queryable where ((property1 == "val1" || property1 == "val2") && property2 == "val3")
+        /// </summary>
+        /// <param name="queryable">the source data to filter</param>
+        /// <param name="entity">the entity type of the iqueryable</param>
+        /// <param name="parameters">parameter criteria</param>
+        /// <returns></returns>
+        public static IQueryable<object> FilterByEntityParameters(IQueryable<object> queryable, IEntityType entity, IDictionary<string, string[]> parameters)
         {
-            // end goal is to create `.Where(entity => ...some condition based on parameters...)`
-            ParameterExpression entityParameter = Expression.Parameter(entity.ClrType, "entity");
-            // create nested list. ?prop1=valA&prop1=valB&prop2=valC becomes [[prop1 == valA, prop1 == valB], [prop2=valC]]
-            var searchCriteria = parameters.Select(property =>
-            {
-                Expression propertyGetter = Expression.Property(entityParameter, Reflection.GetClrPropertyForEntity(entity, property.Key));
-                return property.Value.ToArray().Select(value =>
-                {
-                    return Expression.Equal(propertyGetter, Expression.Constant(Utility.Convert(value, propertyGetter.Type)));
-                });
-            });
-            // assemble nested list into (prop1 == valA || prop1 == valB) && prop2=valC
-            var condition = searchCriteria.Select(or => or.Aggregate(Expression.OrElse)).Aggregate(Expression.AndAlso);
+            // end-goal is to create the expression `.Where(entity => ...some condition based on parameters...)`
 
-            var lambda = Expression.Lambda(condition, new[] { entityParameter });
-            var whereClause = Expression.Call(typeof(Queryable), "Where", new[] { allData.ElementType }, allData.Expression, lambda);
-            return (IQueryable<object>)allData.Provider.CreateQuery(whereClause);
+            // this is our lambda parameter
+            ParameterExpression entityParameter = Expression.Parameter(entity.ClrType, "entity");
+
+            // this is our lambda body
+            var searchCriteria = parameters
+                // create nested list of boolean expressions.
+                // { "property1": ["val1", "val2"], "property2": ["val3"]} becomes [[property1 == val1, property1 == val2], [property2 == val3]]
+                .Select(property =>
+                {
+                    Expression propertyGetter = Expression.Property(entityParameter, Reflection.GetClrPropertyForEntity(entity, property.Key));
+                    return property.Value.Select(value =>
+                    {
+                        return Expression.Equal(propertyGetter, Expression.Constant(Utility.Convert(value, propertyGetter.Type)));
+                    });
+                })
+                // assemble nested booleans into a single boolean expression: (property1 == val1 || property1 == val2) && property2 == val3
+                .Select(or => or.Aggregate(Expression.OrElse)).Aggregate(Expression.AndAlso);
+
+            // assemble lambda, use as the condition in a where clause invoked on our iqueryable
+            var lambda = Expression.Lambda(searchCriteria, entityParameter);
+            var whereClause = Expression.Call(typeof(Queryable), "Where", new[] { queryable.ElementType }, queryable.Expression, lambda);
+            return (IQueryable<object>)queryable.Provider.CreateQuery(whereClause);
         }
     }
 }
