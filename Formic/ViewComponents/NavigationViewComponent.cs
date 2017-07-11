7e63c35700ed5b537a31a66949855ed288df3f9d
Will Fuqua <wafuqua@gmail.com>
2017-07-11 22:20:26 +0700

    cache navigation menu generation

--- Formic/ViewComponents/NavigationViewComponent.cs
+++ Formic/ViewComponents/NavigationViewComponent.cs
@@ -4,26 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Formic.ViewComponents
 {
     public class NavigationViewComponent : ViewComponent
     {
-        private readonly DbContext dbContext;
+        private static IReadOnlyList<string> entities;
 
         public NavigationViewComponent(DbContext context)
         {
-            this.dbContext = context;
+            if(entities == null)
+            {
+                entities = context.Model
+                    .GetEntityTypes()
+                    .Select(type => type.Name)
+                    .ToArray();
+            }
         }
 
         public Task<IViewComponentResult> InvokeAsync()
         {
-            string[] tables = dbContext.Model
-                .GetEntityTypes()
-                .Select(type => type.Name)
-                .ToArray();
             string selectedTable = this.ViewContext.ModelState["table"]?.RawValue as string;
-            var viewModel = new NavigationViewModel(tables, selectedTable);
+            var viewModel = new NavigationViewModel(entities, selectedTable);
             return ViewResult(viewModel);
         }
 
@@ -33,12 +36,12 @@ namespace Formic.ViewComponents
 
     public class NavigationViewModel
     {
-        public NavigationViewModel(string[] tables, string selectedTable)
+        public NavigationViewModel(IReadOnlyList<string> tables, string selectedTable)
         {
             SelectedTable = selectedTable;
             Tables = tables;
         }
         public string SelectedTable { get; }
-        public string[] Tables { get; }
+        public IReadOnlyList<string> Tables { get; }
     }
 }
